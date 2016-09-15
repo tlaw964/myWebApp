2f55fc41c141127ef4b9fe481a6e2b7f07638cb8
Thomas Lawley <Thomas Lawley>
2016-09-15 19:57:09 +1200

    Add project files and folders

---
+++ js/main.js
@@ -0,0 +1,63 @@
+var caption;
+// Get elements from DOM
+var pageheader = $("#page-header")[0]; //note the [0], jQuery returns an object, so to get the html DOM object we need the first item in the object
+var pagecontainer = $("#page-container")[0];
+// The html DOM object has been casted to a input element (as defined in index.html) as later we want to get specific fields that are only avaliable from an input element object
+var imgSelector = $("#my-file-selector")[0];
+var refreshbtn = $("#refreshbtn")[0]; //You dont have to use [0], however this just means whenever you use the object you need to refer to it with [0].
+// Register button listeners
+imgSelector.addEventListener("change", function () {
+    pageheader.innerHTML = "Just a sec while we analyse your photo...";
+    processImage(function (file) {
+        sendCaptionRequest(file, function (caption) {
+            changeUI(); //update the web app
+            //Done!!
+        });
+    });
+});
+function processImage(callback) {
+    var file = imgSelector.files[0]; //get(0) is required as imgSelector is a jQuery object so to get the DOM object, its the first item in the object. files[0] refers to the location of the photo we just chose.
+    var reader = new FileReader();
+    if (file) {
+        reader.readAsDataURL(file); //used to read the contents of the file
+    }
+    else {
+        console.log("Invalid file");
+    }
+    reader.onloadend = function () {
+        //After loading the file it checks if extension is jpg or png and if it isnt it lets the user know.
+        if (!file.name.match(/\.(jpg|jpeg|png)$/)) {
+            pageheader.innerHTML = "Please upload an image file (jpg or png).";
+        }
+        else {
+            //if file is photo it sends the file reference back up
+            callback(file);
+        }
+    };
+}
+function changeUI() {
+    //Show detected mood
+    pageheader.innerHTML = "Your caption is: " + caption;
+}
+function sendCaptionRequest(file, callback) {
+    $.ajax({
+        url: "https://api.projectoxford.ai/vision/v1.0/describe",
+        beforeSend: function (xhrObj) {
+            // Request headers
+            xhrObj.setRequestHeader("Content-Type", "application/octet-stream");
+            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "c3b7d551269843b79a42a1930816909a");
+        },
+        type: "POST",
+        data: file,
+        processData: false
+    })
+        .done(function (data) {
+        //Grab Caption Data
+        caption = data.description.captions[0].text;
+        callback(caption);
+    })
+        .fail(function (error) {
+        pageheader.innerHTML = "Sorry, something went wrong. :( Try again in a bit?";
+        console.log(error.getAllResponseHeaders());
+    });
+}
